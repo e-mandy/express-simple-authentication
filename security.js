f18c0c585b4c8c1046723958fd357d79846ce588
e-mandy <andymfrd02@gmail.com>
2026-01-08 13:57:32 +0100

    La hash avec bcrypt et le sel (utilité et usage)

---
+++ security.js
@@ -0,0 +1,23 @@
+import bcrypt from 'bcrypt';
+
+const securePassword = async (password) => {
+    const salt = 10;
+
+    return await bcrypt.hash(password, salt);
+}
+
+
+const verifyPassword = async (password, hash) => {
+    const result = await bcrypt.compare(password, hash);
+
+    return result;
+}
+
+let hashedPassword = await securePassword("Nitro123");
+
+
+let result = await verifyPassword("Nitro123", hashedPassword);
+console.log(result);
+
+result = await verifyPassword("PasLeBonMotDePasse", hashedPassword);
+console.log(result)
