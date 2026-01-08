d59e12bb7afd004f6cf1b8ba925ab9d4c3f83067
e-mandy <andymfrd02@gmail.com>
2026-01-08 14:49:19 +0100

    Connexion, verification de l'existance du user en bd et comparaison des mots de passe, envoie de message de réussite ou d'erreur

--- server.js
+++ server.js
@@ -39,6 +39,37 @@ router.post('/register', async (req, res) => {
     }
 });
 
+
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    try{
+        const user = await User.findOne({ email });
+        
+        if(!user) return res.status(401).json({
+            code: 401,
+            message: "Identifiants invalides"
+        });
+    
+        const isCorrect = await user.comparePassword(password);
+
+        if(!isCorrect) return res.status(401).json({
+            code: 401,
+            message: "Identifiants invalides"
+        });
+    
+        return res.status(200).json({
+            code: 200,
+            message: "Utilisateur connecté"
+        })
+    }catch(error){
+        return res.status(400).json({
+            code: 400,
+            message: "Connexion échouée"
+        })
+    }
+});
+
 app.listen(3000, () => {
     console.log("The server is listening on the port: 3000");
 });
